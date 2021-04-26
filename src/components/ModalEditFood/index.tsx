5bbe2c7352d939edf5eb503c5f49a506737489c2
Viict0r <viiict0r@hotmail.com>
2021-04-26 17:58:46 -0300

    Resolucao desafio

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -18,11 +18,12 @@ interface IFoodPlate {
 interface IModalProps {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleUpdateFood: (food: Omit<IFoodPlate, 'id' | 'available'>) => void;
+  handleUpdateFood: (food: Omit<IFoodPlate, 'available'>) => void;
   editingFood: IFoodPlate;
 }
 
 interface IEditFoodData {
+  id: number;
   name: string;
   image: string;
   price: string;
@@ -40,8 +41,12 @@ const ModalEditFood: React.FC<IModalProps> = ({
   const handleSubmit = useCallback(
     async (data: IEditFoodData) => {
       // EDIT A FOOD PLATE AND CLOSE THE MODAL
+      Object.assign(data, { id: editingFood.id });
+
+      handleUpdateFood(data);
+      setIsOpen();
     },
-    [handleUpdateFood, setIsOpen],
+    [handleUpdateFood, setIsOpen, editingFood.id],
   );
 
   return (
